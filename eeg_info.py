c122f7d525564984d5b1801a9315be0cf8af37ed
bairan42 <rbai@csdslab.net>
2018-12-03 15:59:31 +0800

    cleaning

--- eeg_info.py
+++ eeg_info.py
@@ -1,36 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Wu Jiaqi
+"""
+
 import mne
 import os
-import numpy as np
-from matplotlib import pyplot as plt
 import csv
+import re
 
-data_path='D:\health_control\eyeopen'  #根据实际情况改变路径
-data_path1='D:\health_control\eyeclose'
+def getdate(str):#读取字符串中日期
+    dl = re.findall(r'\d{4}\d{2}\d{2}',str)
+    return(dl)
 
-dirs=os.listdir(data_path)
-dirs1=os.listdir(data_path1)
-filename_open=[]
-filename_close=[]
-for i in dirs:
-    if os.path.splitext(i)[1]==".eeg":
-        filename_open.append(os.path.splitext(i)[0])
-
-dirs1=os.listdir(data_path1)
-for i in dirs1:
-    if os.path.splitext(i)[1]==".eeg":
-        filename_close.append(os.path.splitext(i)[0])
-        
-fileread=open('eeg_info.csv','w',newline='')
-writer=csv.writer(fileread)
-
-writer.writerow(['id','n_channels*n_times','events'])
-for i in filename_open:
-    raw=mne.io.read_raw_brainvision(data_path+'\\'+i+'.vhdr')
-    a=str(raw)
-    data=[i,a[a.index(':')+1:a.index(')')+1],raw.info['events']]
-    writer.writerow(data)
-for i in filename_close:
-    raw=mne.io.read_raw_brainvision(data_path1+'\\'+i+'.vhdr')
-    a=str(raw)
-    data=[i,a[a.index(':')+1:a.index(')')+1],raw.info['events']]
-    writer.writerow(data)  
+def create(path,eye,group,name): #path:所在路径，eye：眼睛闭合情况，group：对照组实验组，name：生成csv文件名
+    dirs=os.listdir(path)
+    filename=[]
+    for i in dirs:
+        if os.path.splitext(i)[1]==".eeg":
+            filename.append(os.path.splitext(i)[0])
+    fileread=open(name+'.csv','w',newline='')
+    writer=csv.writer(fileread)
+    writer.writerow(['group(control/patient)','id','date','eyes(open/close)', 'num_channel','duration(s)','events'])
+    for i in filename:
+        raw=mne.io.read_raw_brainvision(path+'\\'+i+'.vhdr')
+        a=str(raw)
+        b=a[a.index(':'):a.index(')')+1]
+        data=[group,i,getdate(a),eye,b[b.index(':')+1:b.index('x')],b[b.index('(')+1:b.index(')')],raw.info['events']]
+        writer.writerow(data)  
+path='D:\eeg\eegData\health_control\eyeopen'
+eye='open'
+group='control'
+name='eeg_info_co'
+create(path,eye,group,name)
+#由于目录只有两层，就没有按照文件递归来写，总共生成4个csv文件可以合并来得到最后结果
