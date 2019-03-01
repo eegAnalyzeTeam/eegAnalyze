615f2fdd8d88e0add4d08fdd1bf38ba9b082089b
dongfangyier <1536370121@qq.com>
2019-03-01 09:37:58 +0800

    去掉alpha2中一些没用的注释

--- features/alpha2_110/eeg_classify_model.py
+++ features/alpha2_110/eeg_classify_model.py
@@ -27,11 +27,6 @@ def get_xy(name):
     del csv_data['Unnamed: 0']
     x = np.array(csv_data, dtype=float)
 
-    # y=csv_data.loc[:,'type']
-    # x = np.array(csv_data, dtype=float)[:,:-1]
-    # y = np.array(y, dtype=float)
-    # print(x)
-    # print(y)
 
     return x, y
 
@@ -41,20 +36,6 @@ def split_data(name,i=1):
     x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=i, train_size=0.7)
     return x_train, x_test, y_train.ravel(), y_test.ravel()
 
-
-# # 贝叶斯 多项式模型
-# def naive_bayes_MultinomialNB(i=1):
-#     x_train, x_test, y_train, y_test = split_data(i)
-#     clf = MultinomialNB()
-#     clf.fit(x_train, y_train)
-#
-#     expected = y_test
-#     predicted = clf.predict(x_test)
-#     print(metrics.classification_report(expected, predicted))  # 输出分类信息
-#     label = list(set(y_train))  # 去重复，得到标签类别
-#     print(metrics.confusion_matrix(expected, predicted, labels=label))  # 输出混淆矩阵信息
-#
-#     return clf.score(x_test, y_test)
 
 
 # 贝叶斯 高斯模型
@@ -147,20 +128,6 @@ def random_forest(name,i=1):
     return clf.score(x_test, y_test),accuracy_little,accuracy_big
 
 
-# # 线性回归
-# def linear_regression(i=1):
-#     x_train, x_test, y_train, y_test = split_data(i)
-#     clf = LinearRegression()
-#     clf.fit(x_train, y_train)
-#
-#     expected = y_test
-#     predicted = clf.predict(x_test)
-#     print(metrics.classification_report(expected, predicted))  # 输出分类信息
-#     label = list(set(y_train))  # 去重复，得到标签类别
-#     print(metrics.confusion_matrix(expected, predicted, labels=label))  # 输出混淆矩阵信息
-#
-#     return clf.score(x_test, y_test)
-
 
 def init_main(name):
     colums=['svm','decide_tree','naive_bayes_GaussianNB','random_forest']
@@ -206,86 +173,9 @@ def init_main(name):
     acc_pd.loc['mean'] = acc_pd.mean()
     acc_pd.to_csv(name[:-4]+'_classify_acc_test.csv')
 
-file_names=['select_features_VarianceThreshold_test.csv']
-# file_names=['tsfresh_filteredFeatures.csv','test_sklearn_SelectFromModel.csv','select_features_VarianceThreshold.csv','test_sklearn_ExtraTreesClassifier.csv']
+# file_names=['select_features_VarianceThreshold_test.csv']
+file_names=['tsfresh_filteredFeatures.csv','test_sklearn_SelectFromModel.csv','select_features_VarianceThreshold.csv','test_sklearn_ExtraTreesClassifier.csv']
 for x in file_names:
     init_main(x)
-# decide_tree()
-# naive_bayes_GaussianNB()
-# svm_train()
-# naive_bayes_MultinomialNB()
-# random_forest()
-# for i in range(30):
-#     decide_tree(i)
 
 
-def test_svm_c():
-    x,y=get_xy()
-    x_list=[]
-    y_list=[]
-    for i in range(1,201):
-        count=0.01*i
-        print(count)
-        x_list.append(count)
-        clf = svm.SVC(C=count,kernel='linear', class_weight='balanced')
-        scores = cross_val_score(clf, x, y, cv=8) # 8折交叉验证
-        score=scores.mean()
-        y_list.append(score)
-        print(score)
-
-    # plt.plot(x_list,y_list,label='svm_alpha2')
-    #
-    # x, y = get_xy()
-    # x_list = []
-    # y_list = []
-    # for i in range(1, 201):
-    #     count = 0.01 * i
-    #     print(count)
-    #     x_list.append(count)
-    #     clf = svm.SVC(C=count, kernel='linear', class_weight='balanced')
-    #     scores = cross_val_score(clf, x, y, cv=8)  # 8折交叉验证
-    #     score = scores.mean()
-    #     y_list.append(score)
-    #     print(score)
-    #
-    # plt.plot(x_list, y_list, label='svm_alpha1')
-    #
-    # x, y = get_xy()
-    # x_list = []
-    # y_list = []
-    # for i in range(1, 201):
-    #     count = 0.01 * i
-    #     print(count)
-    #     x_list.append(count)
-    #     clf = svm.SVC(C=count, kernel='linear', class_weight='balanced')
-    #     scores = cross_val_score(clf, x, y, cv=8)  # 8折交叉验证
-    #     score = scores.mean()
-    #     y_list.append(score)
-    #     print(score)
-    #
-    # plt.plot(x_list, y_list, label='svm_all')
-    #
-    # plt.legend(loc='best')
-    # plt.xlabel('c value')
-    # plt.ylabel('score')
-    # plt.title('svm score')
-    #
-    # plt.savefig('test_svm_linear.png')
-    # plt.close()
-
-# test_svm_c()
-
-
-# def LogisticRegression_train():
-#     x, y=get_xy()
-#     y=y.ravel()
-#     clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial')
-#     print(x)
-#     print(y)
-#
-#
-#     scores = cross_val_score(clf, x, y, cv=8)  # 8折交叉验证
-#     print(scores)
-#     print(np.mean(scores))
-#
-# LogisticRegression_train()
