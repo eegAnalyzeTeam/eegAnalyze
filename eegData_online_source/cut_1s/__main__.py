99c19f6a8b50f629c7de4bf131c3c812fda5daae
dongfangyier <1536370121@qq.com>
2019-04-11 14:27:21 +0800

    整理

--- eegData_online_source/cut_1s/__main__.py
+++ eegData_online_source/cut_1s/__main__.py
@@ -10,6 +10,6 @@ eeg_calcFeatures.start()  # 计算特征
 all_select_features.start()  # 选取特征
 handle_multi_csv.start()  # 将多个个体合成一个整表
 all_classify_model_c_k.start()  # k折交叉验证
-test_k_cv_3.start()  # 召回率、精确率、准确率
+test_k_cv_3.start()  # 利用70%数据交叉验证，另30%数据重新验证
 all_learn_curve.start()  # 学习曲线
 eeg_analyze_features.start()  # 对好的特征进行分析
