d6d6f0d7c11c4a518eb569f79a93b1b74a46cee1
dongfangyier <1536370121@qq.com>
2019-05-16 09:55:27 +0800

    add read file

---
+++ fractality_analysis/read_file.py
@@ -0,0 +1,99 @@
+# 此文件主要完成了对eeg文件的读取
+# 输入参数：目录路径
+# 返回:(list)正常人raw，(list)病人raw，(list)通道名称，(list)坏道名称
+
+import mne
+import os
+import check_file
+
+
+def get_raw_info(filePath):
+    raw = mne.io.read_raw_brainvision(filePath + '/health_control/eyeclose/jkdz_cc_20180430_close.vhdr',
+                                      preload=True)
+    print()
+    channel_names = []
+    for i in raw.info['ch_names']:
+        if i != 'Oz':
+            if i != 'ECG':
+                channel_names.append(i)
+
+    bad_channels = ['Oz', 'ECG']
+    return channel_names, bad_channels
+
+
+def troublesome_data(filePath):
+    control_q = []
+    patient_q = []
+    for dirpath, dirs, files in os.walk(filePath):
+
+        if 'eyeclose' in dirpath and 'health_control' in dirpath:
+            # health control group
+            for fname in files:
+                if '.vhdr' in fname:
+                    id_control = fname[:-5]
+                    vmrkf, eegf = check_file.get_vhdr_info(dirpath + '/' + fname)
+                    if vmrkf == eegf and vmrkf == id_control:
+                        print('OK')
+                    else:
+                        control_q.append(id_control)
+
+        elif 'eyeclose' in dirpath and 'mdd_patient' in dirpath:
+            # mdd group
+            for fname in files:
+                if '.vhdr' in fname:
+                    id_patient = fname[:-5]
+                    vmrkf, eegf = check_file.get_vhdr_info(dirpath + '/' + fname)
+                    if vmrkf == eegf and vmrkf == id_patient:
+                        print('OK')
+                    else:
+                        patient_q.append(id_patient)
+
+    return control_q, patient_q
+
+
+def read_data(filePath):
+    # q contains troublesome eeg files. skip them for now
+    control_q, patient_q = troublesome_data(filePath)
+    # q = ['njh_after_pjk_20180725_close.vhdr', 'ccs_yb_20180813_close.vhdr', 'njh_before_pjk_20180613_close.vhdr', 'ccs_before_wjy_20180817_close.vhdr', 'ccs_after_csx_20180511_close.vhdr']
+    print(patient_q)
+    print('---------===========-----------')
+    control_raw = {}
+    patient_raw = {}
+
+    for dirpath, dirs, files in os.walk(filePath):
+        if 'eyeclose' in dirpath and 'health_control' in dirpath:
+            # health control group
+            for fname in files:
+                if '.vhdr' in fname and fname not in control_q:
+                    id_control = fname[:-5]
+
+                    raw = mne.io.read_raw_brainvision(dirpath + '/' + fname, preload=False)
+                    if len(raw.info['ch_names']) == 65:
+                        raw.set_montage(mne.channels.read_montage("standard_1020"))
+                        control_raw[id_control] = raw
+                    else:
+                        print("Abnormal data with " + str(len(raw.info['ch_names'])) + " channels. id=" + id_control)
+
+        elif 'eyeclose' in dirpath and 'mdd_patient' in dirpath:
+            # mdd group
+            for fname in files:
+                if '.vhdr' in fname and fname[:-5] not in patient_q:
+                    id_patient = fname[:-5]
+
+                    raw = mne.io.read_raw_brainvision(dirpath + '/' + fname, preload=False)
+
+                    if len(raw.info['ch_names']) == 65:
+                        raw.set_montage(mne.channels.read_montage("standard_1020"))
+                        patient_raw[id_patient] = raw
+                    else:
+                        print("Abnormal data with " + str(len(raw.info['ch_names'])) + " channels. id=" + id_patient)
+
+    return control_raw, patient_raw
+
+
+def read_file(filePath = '/home/rbai/eegData'):
+    control_raw, patient_raw = read_data(filePath)
+    channel_names, bad_channels = get_raw_info(filePath)
+    return control_raw, patient_raw, channel_names, bad_channels
+
+
